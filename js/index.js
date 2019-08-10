4a8327dc7b50f8b3d75a53c39f95c4d3dc7d5062
aqi12345 <53959319+Aqi1234@users.noreply.github.com>
2019-08-10 17:04:12 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+    //获取屏幕宽度(viewport)
+    let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+
+    // 获取视窗的高度
+    let htmlDom = document.getElementsByTagName("html")[0];
+    
+    //设置html的fontsize
+    htmlDom.style.fontSize = htmlWidth / 10 + "px";
+    
+    //当屏幕进行缩放时，添加resize事件
+    window.addEventListener("resize",(e) => {
+        let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+        htmlDom.style.fontSize = htmlWidth / 10 + "px";
+    })
